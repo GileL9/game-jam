37e5ebabbc9e98f9949ab75a7036fa530f54fcc8
unknown <dragisadjuric711@outlook.com>
2026-06-13 20:40:03 +0200

    asseti dodati

--- Platform/platform_trigger.gd
+++ Platform/platform_trigger.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 func _ready() -> void:
+	pass
 	# Connect the collision signal to our function below when the level starts
-	body_entered.connect(_on_body_entered)
+#	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
 	# 1. Check if the object that entered is actually a platform
