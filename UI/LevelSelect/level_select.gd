c35c3f94168fb8c66a69296c1f8fecf847f1d43e
vanzemunac620 <anastasijalazic05@gmail.com>
2026-06-14 16:02:45 +0200

    napravljen Credits ekran

--- UI/LevelSelect/level_select.gd
+++ UI/LevelSelect/level_select.gd
@@ -45,3 +45,12 @@ func _on_level_6_pressed() -> void:
 func _on_level_4_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/Level4.tscn")
 	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/LevelSelect/Credits.tscn")
+
+func _on_explorer_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/LevelSelect/LevelSelect.tscn")
